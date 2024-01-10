17f60f68bcb74946e12d286646e40c4e35a48164
Iamdilip93 <Dilipvr01@gmail.com>
2024-01-10 13:54:06 +0530

    modify test2

--- src/test/java/testCases/Test2.java
+++ src/test/java/testCases/Test2.java
@@ -7,14 +7,14 @@ public class Test2 {
 	@Test(priority=1)
 	public void test_search() {
 		
-		System.out.println("Search Test Passed....");
+		System.out.println("Search Test Passed........");
 		Assert.assertTrue(true);
 	}
 	
 	@Test(priority=2)
 	public void test_advSearch() {
 		
-		System.out.println("Advanced search Test Passed....");
+		System.out.println("Advanced search Test Passed......");
 		Assert.assertTrue(true);
 	}
 
